ea00a027cd528c31e3532d4ea577614eb84baa01
anthony-995 <mitreski_antonio@yahoo.com>
2026-07-13 22:20:08 +0200

    chore: add GSAP and ScrollTrigger dependencies

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,2 +1,6 @@
 import '../../css/src/index.scss';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 console.log('9024 Media Theme JS Loaded');
