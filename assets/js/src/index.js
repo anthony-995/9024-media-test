f5470511c846bc761dcff1e41587bb06fb936645
anthony-995 <mitreski_antonio@yahoo.com>
2026-07-14 13:47:39 +0200

    feat: implement page loader animation and integrate custom SVG logo

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -3,4 +3,26 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
-console.log('9024 Media Theme JS Loaded');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const loader = document.getElementById('loader');
+  if (loader) {
+    const tl = gsap.timeline({
+      onComplete: () => {
+        loader.style.display = 'none';
+      }
+    });
+
+    tl.to('.loader-logo-row', {
+      xPercent: -105,
+      duration: 1.4,
+      ease: 'power4.inOut',
+      delay: 0.8
+    })
+    .to(loader, {
+      yPercent: -100,
+      duration: 1.0,
+      ease: 'power4.inOut'
+    }, '-=0.5');
+  }
+});
--- header.php
+++ header.php
@@ -8,17 +8,36 @@
 <body <?php body_class( 'bg-white text-brand-black antialiased' ); ?>>
 <?php wp_body_open(); ?>
 
+<div id="loader" class="fixed inset-0 bg-brand-black z-[9999] flex items-center justify-center">
+	<div class="loader-mask overflow-hidden px-1">
+		<div class="loader-logo-row flex gap-4">
+			<svg viewBox="0 0 41.4365 41.4366" class="loader-box w-20 h-20 fill-current text-white select-none">
+				<path d="M41.4365 0V41.4366H0V0H41.4365ZM32.2289 32.2284V9.20818H9.20816V23.0197H27.6245V27.6241H9.20816V32.2279L32.2289 32.2284ZM27.6245 13.8125V18.4164H13.8125V13.8125H27.6245Z" />
+			</svg>
+			<svg viewBox="44.8398 0 41.4914 41.4923" class="loader-box w-20 h-20 fill-current text-white select-none">
+				<path d="M86.3312 0V41.4923H44.8398V0H86.3312ZM77.111 32.2716V9.22067H54.0596V32.2716H77.111ZM58.67 27.6615V13.8308H72.5009V27.6615H58.67Z" />
+			</svg>
+			<svg viewBox="0 44.8555 41.4361 41.5312" class="loader-box w-20 h-20 fill-current text-white select-none">
+				<path d="M41.4361 44.8555V86.3867H0.000976562V44.8555H41.4361ZM9.1366 58.6992H27.5923V63.3121H9.1366V77.1559H32.2072V72.543H13.7496V67.9299H32.2072V54.0862H9.1366V58.6992Z" />
+			</svg>
+			<svg viewBox="44.8398 44.8555 41.4991 41.5475" class="loader-box w-20 h-20 fill-current text-white select-none">
+				<path d="M86.3389 44.8555V86.403H44.8398V44.8555H86.3389ZM54.072 54.0881V67.9371H72.5378V77.1703H77.1542V54.0881H72.5378V63.3208H58.6884V54.0881H54.072Z" />
+			</svg>
+		</div>
+	</div>
+</div>
+
 <header id="masthead" class="site-header absolute top-0 left-0 w-full z-50 bg-transparent">
 	<div class="max-w-[1440px] mx-auto px-6 py-6 md:px-12 flex justify-between items-center">
 		
 		<div class="site-branding">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="inline-block" rel="home">
-				<div class="w-12 h-12 bg-brand-black text-white flex flex-wrap p-1 font-heading font-bold select-none border-2 border-solid border-white">
-					<div class="w-1/2 h-1/2 flex items-center justify-center text-sm leading-none">9</div>
-					<div class="w-1/2 h-1/2 flex items-center justify-center text-sm leading-none">0</div>
-					<div class="w-1/2 h-1/2 flex items-center justify-center text-sm leading-none">2</div>
-					<div class="w-1/2 h-1/2 flex items-center justify-center text-sm leading-none">4</div>
-				</div>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="inline-block text-white hover:text-brand-orange transition-colors duration-300" rel="home">
+				<svg width="48" height="48" viewBox="0 0 87 87" fill="none" xmlns="http://www.w3.org/2000/svg" class="block">
+					<path d="M41.4365 0V41.4366H0V0H41.4365ZM32.2289 32.2284V9.20818H9.20816V23.0197H27.6245V27.6241H9.20816V32.2279L32.2289 32.2284ZM27.6245 13.8125V18.4164H13.8125V13.8125H27.6245Z" fill="currentColor"/>
+					<path d="M86.3312 0V41.4923H44.8398V0H86.3312ZM77.111 32.2716V9.22067H54.0596V32.2716H77.111ZM58.67 27.6615V13.8308H72.5009V27.6615H58.67Z" fill="currentColor"/>
+					<path d="M41.4361 44.8555V86.3867H0.000976562V44.8555H41.4361ZM9.1366 58.6992H27.5923V63.3121H9.1366V77.1559H32.2072V72.543H13.7496V67.9299H32.2072V54.0862H9.1366V58.6992Z" fill="currentColor"/>
+					<path d="M86.3389 44.8555V86.403H44.8398V44.8555H86.3389ZM54.072 54.0881V67.9371H72.5378V77.1703H77.1542V54.0881H72.5378V63.3208H58.6884V54.0881H54.072Z" fill="currentColor"/>
+				</svg>
 			</a>
 		</div>
 		
